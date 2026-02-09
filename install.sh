185aa4f51a041cc68c4a8ab15109ed0bc5d6bf5a
jrthekidRSS <jrthekidrs@proton.me>
2026-02-09 14:46:01 -0600

    minimal additions to 'install.sh'

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_DIR="$(dirname "$0")"
+
+print-help() {
+cat <<- EOF
+Usage: $SCRIPT_NAME [-ch]
+EOF
+}
+
 while (( $# > 0 )); do
-    echo yes
+    case "$1" in
+        -h|--help)
+            print-help
+            exit 0
+            ;;
+        *)
+            printf "error: invalid parameter '%s'\n%s\n" "$1" "$(print-help)" 1>&2
+    esac
+
     shift
 done
+
